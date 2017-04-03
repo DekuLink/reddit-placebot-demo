e129c53c27540d1bd171234e5dc2808cc2447b36
DekuLink <DekuLink@users.noreply.github.com>
2017-04-03 04:09:14 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  BOARD_URL: 'https://www.reddit.com/api/place/board-bitmap',
+  BOARD_FILE: __dirname + '/tmp/board.bmp',
+
+  REMOTE_TARGET_URL: 'https://github.com/DekuLink/reddit-placebot-raocow/raw/master/target.bmp',
+  REMOTE_TARGET_FILE: __dirname + '/tmp/remote_target',
+
+  LOCAL_TARGET_FILE: __dirname + '/images/target.png',
+
+  DRAW_URL: 'https://www.reddit.com/api/place/draw.json',
+
+  // Use the REMOTE_TARGET_URL file as target, otherwise it's gonna just
+  // try to read from target.bmp
+  useRemoteTarget: true,
+
+  // Wait until these amount of accounts are available
+  // and paint pixels at the same time
+  bundleAccounts: 1,
+
+  // The PLACEBOT mark in the world <3
+  targetStartX: 0,
+  targetStartY: 0,
+
+  drawMode: 'RANDOM', // TOPLEFT | RANDOM
+
+  // Testing configuration things
+
+  // This is used for testing
+  useExistingBoardCache: false,
+
+  // Do not send the painting to the server so you don't waste your pixels
+  // while testing something else
+  mockPainting: false
+}
